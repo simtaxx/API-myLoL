24a308cea1739017e3c278be061b58666a114c5c
Haris Souici <haris@MacBook-Pro-de-Haris.local>
2020-08-29 11:50:18 +0200

    router - optimize code

--- src/app.js
+++ src/app.js
@@ -1,45 +1,18 @@
 // Import library
 import express from 'express'
-import { port } from './config/config'
+import { port, cors } from './config/config'
 import path from 'path'
-import cors from 'cors'
-import axios from 'axios'
-import { apiKey } from './config/config'
+import router from './config/router'
 
 const app = express() // init express
 
-app.use(cors())
-
+app.use(cors)
+app.use(router)
 // Configuration du moteur de rendu
-app.set("view engine", "ejs");
-
+app.set("view engine", "ejs")
 // Définition du dossier static du client
-app.set("views", __dirname + "/www");
-app.use(express.static(path.join(__dirname, "www")));
-
-const router = express.Router()
-const euUrl = 'https://euw1.api.riotgames.com'
-const playerIdUrl = '/lol/summoner/v4/summoners/by-name/'
-const playerDataUrl = '/lol/league/v4/entries/by-summoner/'
-
-router.get('/', (req, res) => {
-  res.render('index')
-})
-
-app.get('/player', async (req, res) => {
-  const username = req.query.username
-  console.log(username)
-  try {
-    const userProfil = await axios.get(euUrl + playerIdUrl + username, { params: { api_key: apiKey  } })
-    const userId = userProfil.data.id
-    const userData = await axios.get(euUrl + playerDataUrl + userId, { params: { api_key: apiKey  } })
-    console.log('try', userData)
-    res.json({ data: userData.data })
-  } catch (error) {
-    console.log(error)
-    return error
-  }
-})
+app.set("views", __dirname + "/www")
+app.use(express.static(path.join(__dirname, "www")))
 
 app.listen(port, () => {
   console.log(`the url is http://localhost:${port}`)
--- src/config/router.js
+++ src/config/router.js
@@ -3,9 +3,12 @@ import axios from 'axios'
 import { apiKey } from './config'
 
 const router = express.Router()
-const euUrl = 'https://euw1.api.riotgames.com/'
+const euUrl = 'https://euw1.api.riotgames.com'
 const playerIdUrl = '/lol/summoner/v4/summoners/by-name/'
-const playerDataUrl = 'lol/league/v4/entries/by-summoner/'
+const playerDataUrl = '/lol/league/v4/entries/by-summoner/'
+
+const getUser = (username) => axios.get(euUrl + playerIdUrl + username, { params: { api_key: apiKey  } })
+const getUserData = (userId) => axios.get(euUrl + playerDataUrl + userId, { params: { api_key: apiKey  } })
 
 router.get('/', (req, res) => {
   res.render('index')
@@ -13,16 +16,14 @@ router.get('/', (req, res) => {
 
 router.get('/player', async (req, res) => {
   const username = req.query.username
-  console.log(username)
   try {
-    const userProfil = await axios.get(euUrl + playerIdUrl + username, { params: { api_key: apiKey  } })
+    const userProfil = await getUser(username)
     const userId = userProfil.data.id
-    const userData = await axios.get(euUrl + playerDataUrl + userId, { params: { api_key: apiKey  } })
-    res.render('index')
-    return userData
+    const userData = await getUserData(userId)
+    res.json({ data: userData.data })
   } catch (error) {
-    console.log(error.response.status)
-    res.render('index')
+    console.log(error.response)
+    return error
   }
 })
 
